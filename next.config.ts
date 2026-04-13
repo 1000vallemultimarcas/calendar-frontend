996981930506ec22f5e5ad0a23d23d724575d505
ahBuin <ah.buin@1000vallemultimarcas.com.br>
2026-04-13 11:52:56 -0300

    feat: Install and configuration of Docker

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
 import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
-const nextConfig : NextConfig = {
-  /* config options here */
-    // experimental : {
-    //     ppr : "incremental"
-    // },
-    reactStrictMode: true,
-    serverExternalPackages: ["twoslash", "typescript"],
+const nextConfig: NextConfig = {
+  output: "standalone",
+  reactStrictMode: true,
+  serverExternalPackages: ["twoslash", "typescript"],
 };
 
 export default nextConfig;
